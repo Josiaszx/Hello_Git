e3ce39634a6865e97dd5cf4ce124748489a2b492
Josias <jsmdndev@gmail.com>
2025-05-11 16:22:25 -0300

    Modificacion en HelloGit-3 desde ramificacion_1

--- HelloGit-3.py
+++ HelloGit-3.py
@@ -2,4 +2,5 @@
 print("hola")
 
 #hacemos una modifcacion para hacer un merge con ramificacion_1
-print("Print desde Main")
+print("Print desde Main v3")
+#modificamos desde ramificacion_1 para generar conflicto con la modificacion de main
